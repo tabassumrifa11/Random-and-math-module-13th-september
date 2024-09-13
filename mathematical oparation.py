2099bf11ca3264518f2d8678f87aa0830780f2c9
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-13 17:44:32 +0600

    tiktiki

---
+++ mathematical oparation.py
@@ -0,0 +1,14 @@
+print('\033c')
+
+import math
+
+print( math.sin(45) )
+print( math.cos(45) )
+print( math.tan(45) )
+
+print()
+
+
+print( math.ceil(4.12) )
+print( math.floor(4.99) )
+print( math.factorial(5) )
